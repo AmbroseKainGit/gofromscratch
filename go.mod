1f635730ee6720a969f1a0353a51eff02a1d8445
Cristian Ceballos <cmceballos88@gmail.com>
2023-06-25 13:04:17 +0200

    Hello world in go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AmbroseKainGit/gofromscratch
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,10 @@
+// Everything start with package (go convention)
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello world")
+}
