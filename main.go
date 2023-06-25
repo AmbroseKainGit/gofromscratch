dcb31a76ceec6456472813b4fd9baad0d52d13a7
Cristian Ceballos <cmceballos88@gmail.com>
2023-06-25 14:53:38 +0200

    If and else in go and switch in go with inline declaration in if and switch

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/AmbroseKainGit/gofromscratch/variables"
+	"runtime"
 )
 
 // Function that are capitalized are by default global to the package and all the package that import that package
@@ -13,7 +12,28 @@ func main() {
 	// fmt.Println("Hello world")
 	// variables.ShowInts()
 	// variables.AllVariables()
-	estado, text := variables.AnythingToText(1500)
-	fmt.Println(estado)
-	fmt.Println(text)
+	// estado, text := variables.AnythingToText(1500)
+	// fmt.Println(estado)
+	// fmt.Println(text)
+	// os := runtime.GOOS
+	// First declare variable then do the validation
+	if os := runtime.GOOS; os != "windows" {
+		fmt.Println("This is not windosws")
+	} else {
+		fmt.Println("This is  windosws")
+	}
+	// if os := runtime.GOOS; os != "windows" {
+	// 	fmt.Println("This is not windosws is : ", os)
+	// 	return
+	// }
+	// fmt.Println("This is  windosws")
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("linux")
+	case "darwind":
+		fmt.Println("darwind")
+	default:
+		fmt.Printf("%s \n", os)
+	}
+
 }
